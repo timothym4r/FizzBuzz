fc3ca19cc86e6e5002770f61be5d6676f19b0f8b
timothym4r <130308583+timothym4r@users.noreply.github.com>
2023-09-13 11:58:05 -0400

    Adding FizzBuzz.main()

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,12 @@
-package PACKAGE_NAME;public class Main {
+public class Main {
+
+    public static void main(String[] args) {
+        FizzBuzz.doFizzbuzz(5);
+
+        FizzBuzz.main(null);
+
+        // you can also do FizzBuzz.main(new String[0]);
+
+    }
+
 }
